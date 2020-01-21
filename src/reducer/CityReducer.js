574a0b7df0fd6bb2679a0ec425249fe297b93cc1
Aldar Tsydenov <prestige113@yandex.ru>
2020-01-21 13:59:29 +0800

    new action

--- src/reducer/CityReducer.js
+++ src/reducer/CityReducer.js
@@ -1,13 +1,17 @@
 import {ActionsTypes} from './../actions/CityAction';
 
 const initialState = {
-    cities: []
-}
+    cities: [],
+    selectCities: []
+};
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case ActionsTypes.FOUND_CITY: {
-            return {...state, cities: action.payload}
+            return {...state, cities: action.payload};
+        }
+        case ActionsTypes.MONITORING_LOADED: {
+            return {...state, selectCities: action.payload};
         }
         default:
             return state;
--- src/utils/filterCity.js
+++ src/utils/filterCity.js
@@ -2,6 +2,37 @@ import {city} from './russia';
 
 export const filterCity = (value) => {
     return city.filter(c => {
-        return c.city.substring(0, value.length) === value
+        return c.city.substring(0, value.length) === value;
     });
-}
+};
+export const createMonitoringRandom = (name) => {
+    const temperature = Math.random() * 30 * (Math.random() > 0.5 ? -1 : 1);
+
+    const cityTemperature = {
+        temperature: temperature,
+        wind: Math.random() * 10,
+        pressure: 800 - Math.random() * 100,
+        type: typesWeather.find(w => w.t1 < temperature && w.t2 >= temperature),
+        cityName: name
+    };
+};
+
+const typesWeather =
+    [
+        {
+            t1: 5,
+            t2: 50,
+            type: 'sunshine'
+        },
+        {
+            t1: 0,
+            t2: 5,
+            type: 'rain'
+        },
+        {
+            t1: -30,
+            t2: 0,
+            type: 'rain'
+        }
+    ];
+
